663793f20278402beb335976389ca014087b137e
Extrasklep <extrasklep@firemail.cc>
2020-02-14 15:54:23 +0100

    Split parsing and executing into functions

--- main.cpp
+++ main.cpp
@@ -242,6 +242,218 @@ bool isOutputType(unsigned char type) {
     return false;
 }
 
+//returns 0 or 1 if the line is a comment/whitespace/...
+unsigned char splitLine(char* line,unsigned char* elementTypes,char** elementStrings,unsigned char& currentElement) {
+    unsigned long currentLine = gLine;
+    //character position in current line
+    unsigned long linecpos=0;
+    //line length
+    unsigned long linelen=strlen(line);
+    if(debug) {std::cout << '\n' << currentLine << ": " << line;}
+    
+    //start parsing
+    if(debug>=255) {std::cout << '\n';}
+    //set offset to first not space character
+    while(isWhiteSpace(line[linecpos]) && linecpos<linelen) {
+        if(debug>=255) {std::cout << currentLine << ":" << linecpos << " whitespace character\n";}
+        linecpos++;
+    }
+    //end of line reached
+    if(linecpos>=linelen) {
+        //continue;
+        return 1;
+    }
+    //check if line is a comment
+    if(line[linecpos]=='/' || line[linecpos]=='#') {
+        if(debug) {std::cout << "   (commented line)";}
+        //continue;
+        return 1;
+    }
+    //split elements
+    /* element types
+     * 0 = none
+     * 1 = number
+     * 2 = signs (1:>> 2:>+ 3:>- 4:>* 5:>/ 6:>? )
+     * 3 = variable
+     * 4 = terminator
+     * 5 = comment
+     * 6 = output
+     * 7 = input
+     * 8 = raw output
+     * 9 = line number
+     * 10 = escape character
+     * 11 = newline
+     */
+    unsigned char types=11; //amount of different types
+    //unsigned char elementTypes[6]={0,0,0,0,0,0};
+    //char elementStrings[6][32];
+    //unsigned char currentElement=0;
+    unsigned long currentElementStrPos=0;
+    bool newElement=1;
+    bool keepReading=1;
+    //this part is also a mess i guess
+    for(;linecpos<linelen && keepReading;linecpos++) {
+        if(currentElement>sizeof(elementTypes)) {std::cout << '\n' << currentLine << ":" << linecpos+1 << " syntax error: too many elements\n"; exit(255);}
+        //starting new element, find out the type of it
+        if(newElement) {
+            if(debug>=255) {std::cout << currentLine << ":" << linecpos+1 << " trying to start new element\n";}
+            if(isWhiteSpace(line[linecpos])) {
+                if(debug>=255) {std::cout << currentLine << ":" << linecpos+1 << " character is whitespace, trying next\n";}
+                continue;
+            }
+            bool found=0;
+            for(unsigned char i=1;i<=types && found==0;i++) {
+                if(isType(line[linecpos],i,1)) {
+                    if(debug>=255) {unsigned int dei=i; std::cout << currentLine << ":" << linecpos+1 << " element type " << dei << '\n';}
+                    found=1;
+                    if(i==5) {
+                        if(debug>=255) {std::cout << currentLine << ":" << linecpos+1 << " comment\n";}
+                        keepReading=0;
+                        break;
+                    }
+                    newElement=0;
+                    elementTypes[currentElement]=i;
+                    unsigned long elementStringSize=1;
+                    for(unsigned long p=linecpos;p<linelen && isType(line[p],i,0);p++) {elementStringSize++;}
+                    if(elementStringSize>32) {
+                        std::cout << '\n' << currentLine << ":" << linecpos+1 << " syntax error: element too long\n";
+                        exit(255);
+                    }
+                    elementStrings[currentElement][currentElementStrPos++] = line[linecpos];
+                    if(i==4) {
+                        if(debug>=255) {std::cout << currentLine << ":" << linecpos+1 << " terminator\n";}
+                        currentElement++; currentElementStrPos=0;
+                        keepReading=0;
+                    }
+                    break;
+                }
+            }
+        }
+        //read more of current element
+        else {
+            //if current character is no longer of current type
+            if(isType(line[linecpos],elementTypes[currentElement],0)==false || (elementTypes[currentElement]==2 && currentElementStrPos>1) || (elementTypes[currentElement]==10 && currentElementStrPos>1)) {
+                if(debug>=255) {std::cout << currentLine << ":" << linecpos+1 << " element is over\n";} 
+                linecpos--; 
+                elementStrings[currentElement][currentElementStrPos] = '\0';
+                currentElement++; 
+                currentElementStrPos=0; 
+                newElement=1; 
+                continue;
+            }
+            elementStrings[currentElement][currentElementStrPos++] = line[linecpos];
+        }
+    }
+    
+    //print split elements
+    if(debug) {
+        std::cout << "   (";
+        bool firstTime=1;
+        for(unsigned int i=0;i<currentElement;i++) {
+            if(firstTime==false) {std::cout << ", ";} else {firstTime=0;}
+            std::cout << elementStrings[i];
+        }
+        std::cout << ")";
+        if(debug>=200) {std::cout << '\n';}
+    }
+    return 0;
+}
+
+//returns 0 or jump destination
+unsigned long executeLine(unsigned char* elementTypes,char** elementStrings,unsigned char& currentElement) {
+    unsigned long currentLine = gLine;
+    //no terminator
+    if(elementStrings[currentElement-1][0]!=';') {std::cout << '\n' << "syntax error: expected ; on line " << currentLine << '\n'; exit(255);}
+    //not enough elements
+    if(currentElement-1<3) {std::cout << "\nsyntax error: incomplete operation at line " << currentLine << '\n'; exit(255);}
+    
+    // >> write input to output
+    if(isInputType(elementTypes[0]) && getSignType(elementStrings[1])==1 && isOutputType(elementTypes[2])) {
+        long op1=getNumberFrom(elementStrings[0]);
+        putNumberTo(elementStrings[2],op1);
+        if(debug>=200) {std::cout << currentLine << ": action: write " << op1 << " to " << elementStrings[2] << '\n';}
+    }
+    // >+ add input to output
+    else if(isInputType(elementTypes[0]) && getSignType(elementStrings[1])==2 && isOutputType(elementTypes[2])) {
+        long op1=getNumberFrom(elementStrings[0]);
+        long op2=getNumberFrom(elementStrings[2]);
+        putNumberTo(elementStrings[2],op2+op1);
+        if(debug>=200) {std::cout << currentLine << ": action: add " << op1 << " to " << op2 << " at " << elementStrings[2] << '\n';}
+    }
+    // >- subtract input from output
+    else if(isInputType(elementTypes[0]) && getSignType(elementStrings[1])==3 && isOutputType(elementTypes[2])) {
+        long op1=getNumberFrom(elementStrings[0]);
+        long op2=getNumberFrom(elementStrings[2]);
+        putNumberTo(elementStrings[2],op2-op1);
+        if(debug>=200) {std::cout << currentLine << ": action: subtract " << op1 << " from " << op2 << " at " << elementStrings[2] << '\n';}
+    }
+    // >* multiply output by input
+    else if(isInputType(elementTypes[0]) && getSignType(elementStrings[1])==4 && isOutputType(elementTypes[2])) {
+        long op1=getNumberFrom(elementStrings[0]);
+        long op2=getNumberFrom(elementStrings[2]);
+        putNumberTo(elementStrings[2],op2*op1);
+        if(debug>=200) {std::cout << currentLine << ": action: multiply " << op2 << " by " << op1 << " at " << elementStrings[2] << '\n';}
+    }
+    // >/ divide output by input
+    else if(isInputType(elementTypes[0]) && getSignType(elementStrings[1])==5 && isOutputType(elementTypes[2])) {
+        long op1=getNumberFrom(elementStrings[0]);
+        long op2=getNumberFrom(elementStrings[2]);
+        putNumberTo(elementStrings[2],op2/op1);
+        if(debug>=200) {std::cout << currentLine << ": action: divide " << op2 << " by " << op1 << " at " << elementStrings[2] << '\n';}
+    }
+    // >? jump to output(actually an input) if input is bigger than 0
+    else if(isInputType(elementTypes[0]) && getSignType(elementStrings[1])==6 && isInputType(elementTypes[2])) {
+        long op1=getNumberFrom(elementStrings[0]);
+        if(op1>0) {
+            long op2=getNumberFrom(elementStrings[2]);
+            if(op2<1) {
+                std::cout << '\n' << currentLine << ": illegal jump\n";
+                exit(255);
+            }
+            if(debug>=200) {std::cout << gLine << ": action: jump to " << op2 << ": " << op1 << " is bigger than 0 at " << elementStrings[0] << '\n';}
+            return op2;
+        } else {
+            if(debug>=200) {std::cout << currentLine << ": no jump: " << op1 << " is not more than 0 at " << elementStrings[0] << '\n';}
+        }
+    }
+    //not a valid operation
+    else {
+        std::cout << '\n' << currentLine << ": error: invalid operation\n";
+        exit(255);
+    }
+    return 0;
+}
+
+//returns 0 or jump destination
+unsigned long parseLine(char* line) {
+    /* element types
+     * 0 = none
+     * 1 = number
+     * 2 = signs (1:>> 2:>+ 3:>- 4:>* 5:>/ 6:>? )
+     * 3 = variable
+     * 4 = terminator
+     * 5 = comment
+     * 6 = output
+     * 7 = input
+     * 8 = raw output
+     * 9 = line number
+     * 10 = escape character
+     * 11 = newline
+     */
+    unsigned char elementTypes[6]={0,0,0,0,0,0};
+    char* elementStrings[6];
+    //allocate memory for element strings
+    for(unsigned char i=0;i<6;i++) {elementStrings[i] = new char[32];}
+    unsigned char currentElement=0;
+    //return if the line is a comment/whitespace/...
+    if(splitLine(line,elementTypes,elementStrings,currentElement)) return 0;
+    
+    unsigned long res = executeLine(elementTypes,elementStrings,currentElement);
+    //free strings
+    for(unsigned char i=0;i<6;i++) {delete[] elementStrings[i];}
+    return res;
+}
+
 int main(int argc,char* args[]) {
     if(argc<2) {
         std::cout << "Extrasklep's language interpreter version 1.0\nusage: " << args[0] << " [script] [debug level]\n";
@@ -289,181 +501,12 @@ int main(int argc,char* args[]) {
             strcpy(line,sline.c_str());
             gLine=currentLine;
             
-            //character position in current line
-            unsigned long linecpos=0;
-            //line length
-            unsigned long linelen=strlen(line);
-            if(debug) {std::cout << '\n' << currentLine << ": " << line;}
-            
-            //start parsing
-            if(debug>=255) {std::cout << '\n';}
-            //set offset to first not space character
-            while(isWhiteSpace(line[linecpos]) && linecpos<linelen) {
-                if(debug>=255) {std::cout << currentLine << ":" << linecpos << " whitespace character\n";}
-                linecpos++;
-            }
-            //end of line reached
-            if(linecpos>=linelen) {
-                continue;
-            }
-            //check if line is a comment
-            if(line[linecpos]=='/' || line[linecpos]=='#') {
-                if(debug) {std::cout << "   (commented line)";}
-                continue;
-            }
-            //split elements
-            /* element types
-             * 0 = none
-             * 1 = number
-             * 2 = signs (1:>> 2:>+ 3:>- 4:>* 5:>/ 6:>? )
-             * 3 = variable
-             * 4 = terminator
-             * 5 = comment
-             * 6 = output
-             * 7 = input
-             * 8 = raw output
-             * 9 = line number
-             * 10 = escape character
-             * 11 = newline
-             */
-            unsigned char types=11; //amount of different types
-            unsigned char elementTypes[6]={0,0,0,0,0,0};
-            char elementStrings[6][32];
-            unsigned char currentElement=0;
-            unsigned long currentElementStrPos=0;
-            bool newElement=1;
-            bool keepReading=1;
-            //this part is also a mess i guess
-            for(;linecpos<linelen && keepReading;linecpos++) {
-                if(currentElement>sizeof(elementTypes)) {std::cout << '\n' << currentLine << ":" << linecpos+1 << " syntax error: too many elements\n"; exit(255);}
-                //starting new element, find out the type of it
-                if(newElement) {
-                    if(debug>=255) {std::cout << currentLine << ":" << linecpos+1 << " trying to start new element\n";}
-                    if(isWhiteSpace(line[linecpos])) {
-                        if(debug>=255) {std::cout << currentLine << ":" << linecpos+1 << " character is whitespace, trying next\n";}
-                        continue;
-                    }
-                    bool found=0;
-                    for(unsigned char i=1;i<=types && found==0;i++) {
-                        if(isType(line[linecpos],i,1)) {
-                            if(debug>=255) {unsigned int dei=i; std::cout << currentLine << ":" << linecpos+1 << " element type " << dei << '\n';}
-                            found=1;
-                            if(i==5) {
-                                if(debug>=255) {std::cout << currentLine << ":" << linecpos+1 << " comment\n";}
-                                keepReading=0;
-                                break;
-                            }
-                            newElement=0;
-                            elementTypes[currentElement]=i;
-                            unsigned long elementStringSize=1;
-                            for(unsigned long p=linecpos;p<linelen && isType(line[p],i,0);p++) {elementStringSize++;}
-                            if(elementStringSize>32) {
-                                std::cout << '\n' << currentLine << ":" << linecpos+1 << " syntax error: element too long\n";
-                                exit(255);
-                            }
-                            elementStrings[currentElement][currentElementStrPos++] = line[linecpos];
-                            if(i==4) {
-                                if(debug>=255) {std::cout << currentLine << ":" << linecpos+1 << " terminator\n";}
-                                currentElement++; currentElementStrPos=0;
-                                keepReading=0;
-                            }
-                            break;
-                        }
-                    }
-                }
-                //read more of current element
-                else {
-                    //if current character is no longer of current type
-                    if(isType(line[linecpos],elementTypes[currentElement],0)==false || (elementTypes[currentElement]==2 && currentElementStrPos>1) || (elementTypes[currentElement]==10 && currentElementStrPos>1)) {
-                        if(debug>=255) {std::cout << currentLine << ":" << linecpos+1 << " element is over\n";} 
-                        linecpos--; 
-                        elementStrings[currentElement][currentElementStrPos] = '\0';
-                        currentElement++; 
-                        currentElementStrPos=0; 
-                        newElement=1; 
-                        continue;
-                    }
-                    elementStrings[currentElement][currentElementStrPos++] = line[linecpos];
-                }
-            }
-            
-            //print split elements
-            if(debug) {
-                std::cout << "   (";
-                bool firstTime=1;
-                for(unsigned int i=0;i<currentElement;i++) {
-                    if(firstTime==false) {std::cout << ", ";} else {firstTime=0;}
-                    std::cout << elementStrings[i];
-                }
-                std::cout << ")";
-                if(debug>=200) {std::cout << '\n';}
-            }
-            
             //parse and execute
-            
-            //no terminator
-            if(elementStrings[currentElement-1][0]!=';') {std::cout << '\n' << "syntax error: expected ; near " << currentLine << ":" << linecpos+1 << '\n'; exit(255);}
-            //not enough elements
-            if(currentElement-1<3) {std::cout << "\nsyntax error: incomplete operation at line " << currentLine << '\n'; exit(255);}
-            
-            // >> write input to output
-            if(isInputType(elementTypes[0]) && getSignType(elementStrings[1])==1 && isOutputType(elementTypes[2])) {
-                long op1=getNumberFrom(elementStrings[0]);
-                putNumberTo(elementStrings[2],op1);
-                if(debug>=200) {std::cout << currentLine << ": action: write " << op1 << " to " << elementStrings[2] << '\n';}
+            if(jumpTo = parseLine(line)) {
+                jumpRequested=1;
+                currentLine=0;
+                file.seekg(std::ios::beg);
             }
-            // >+ add input to output
-            else if(isInputType(elementTypes[0]) && getSignType(elementStrings[1])==2 && isOutputType(elementTypes[2])) {
-                long op1=getNumberFrom(elementStrings[0]);
-                long op2=getNumberFrom(elementStrings[2]);
-                putNumberTo(elementStrings[2],op2+op1);
-                if(debug>=200) {std::cout << currentLine << ": action: add " << op1 << " to " << op2 << " at " << elementStrings[2] << '\n';}
-            }
-            // >- subtract input from output
-            else if(isInputType(elementTypes[0]) && getSignType(elementStrings[1])==3 && isOutputType(elementTypes[2])) {
-                long op1=getNumberFrom(elementStrings[0]);
-                long op2=getNumberFrom(elementStrings[2]);
-                putNumberTo(elementStrings[2],op2-op1);
-                if(debug>=200) {std::cout << currentLine << ": action: subtract " << op1 << " from " << op2 << " at " << elementStrings[2] << '\n';}
-            }
-            // >* multiply output by input
-            else if(isInputType(elementTypes[0]) && getSignType(elementStrings[1])==4 && isOutputType(elementTypes[2])) {
-                long op1=getNumberFrom(elementStrings[0]);
-                long op2=getNumberFrom(elementStrings[2]);
-                putNumberTo(elementStrings[2],op2*op1);
-                if(debug>=200) {std::cout << currentLine << ": action: multiply " << op2 << " by " << op1 << " at " << elementStrings[2] << '\n';}
-            }
-            // >/ divide output by input
-            else if(isInputType(elementTypes[0]) && getSignType(elementStrings[1])==5 && isOutputType(elementTypes[2])) {
-                long op1=getNumberFrom(elementStrings[0]);
-                long op2=getNumberFrom(elementStrings[2]);
-                putNumberTo(elementStrings[2],op2/op1);
-                if(debug>=200) {std::cout << currentLine << ": action: divide " << op2 << " by " << op1 << " at " << elementStrings[2] << '\n';}
-            }
-            // >? jump to output(actually an input) if input is bigger than 0
-            else if(isInputType(elementTypes[0]) && getSignType(elementStrings[1])==6 && isInputType(elementTypes[2])) {
-                long op1=getNumberFrom(elementStrings[0]);
-                if(op1>0) {
-                    long op2=getNumberFrom(elementStrings[2]);
-                    //do jump
-                    jumpTo=op2;
-                    if(jumpTo<1) {
-                        std::cout << '\n' << currentLine << ": illegal jump\n";
-                        exit(255);
-                    }
-                    jumpRequested=1;
-                    currentLine=0;
-                    file.seekg(std::ios::beg);
-                    if(debug>=200) {std::cout << gLine << ": action: jump to " << op2 << ": " << op1 << " is bigger than 0 at " << elementStrings[0] << '\n';}
-                } else {
-                    if(debug>=200) {std::cout << currentLine << ": no jump: " << op1 << " is not more than 0 at " << elementStrings[0] << '\n';}
-                }
-            }
-            //not a valid operation
-            else {
-                std::cout << '\n' << currentLine << ": warning: invalid operation - skipping line\n";
-            }
-            
         }
     file.close();
     if(debug) {std::cout << '\n';}
